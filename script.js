0b8b92283c7c250e34fe3fde967a74cd8883b056
Rafael Elias Ioppi <141973836+RafaelEliasIoppi@users.noreply.github.com>
2023-09-24 02:26:04 +0000

    ultima

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-var imgs = document.getElementById('imagem')
-var img = document.querySelectorAll('#imagem img')
-
-let idx = 0
-
-function carrossel(){
-    idx++;
-    if (idx > 11){
-        idx = 0
-    }
-     imgs.style.transform = `translateX(${-idx * 200}px)`
-
+const images = ['1920.png', '1930.png', '1940.png', '1950.png','1960.png',
+'1970.png', '1980.png', '1990.png', '2000.png', '2010.png','2020.png' ];
+''
+function getRandomImage() {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    return images[randomIndex];
 }
-setInterval(carrossel, 1500)
+
+function loadImage() {
+    const imageContainer = document.getElementById('image-container');
+    const image = document.createElement('img');
+    image.src = getRandomImage();
+    image.classList.add('image');
+    imageContainer.appendChild(image);
+}
+
+function loadImagesOnScroll() {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        // Você está no final da página, carregue mais imagens
+        loadImage();
+    }
+}
+
+// Carregue algumas imagens quando a página carregar inicialmente
+for (let i = 0; i < 5; i++) {
+    loadImage();
+}
+
+// Registre o evento de scroll
+window.addEventListener('scroll', loadImagesOnScroll);
